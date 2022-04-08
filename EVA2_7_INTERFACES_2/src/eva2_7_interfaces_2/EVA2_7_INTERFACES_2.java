d1c6d07b5917121999727550a673e5a29efe33ac
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-04-08 10:07:32 -0500

    Práctica 7_Interfaces_2

---
+++ EVA2_7_INTERFACES_2/src/eva2_7_interfaces_2/EVA2_7_INTERFACES_2.java
@@ -0,0 +1,28 @@
+package eva2_7_interfaces_2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @author José Sebastian López Ibarra
+ * Wednesday March 30 2022
+ */
+
+public class EVA2_7_INTERFACES_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // Array, List, ArrayList, Stack (Pilas), Queus (Colas)
+        // Collections - Store Data
+        ArrayList myArrayList = new ArrayList();
+        //myArrayList.
+        LinkedList myLinkedList = new LinkedList();
+        //myLinkedList. 
+        Stack myStack = new Stack();
+        //myStack.
+    }    
+}
